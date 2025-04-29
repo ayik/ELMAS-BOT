6a09d26d65f4708826badc49bb2b1929d81ed15f
Obed tech <obedomoke18@gmail.com>
2025-04-29 10:29:51 +0300

    Update dl-img.js

--- plugins/dl-img.js
+++ plugins/dl-img.js
@@ -37,7 +37,7 @@ cmd({
                 from,
                 { 
                     image: { url: imageUrl },
-                    caption: `📷 Result for: ${query}\n> © Powered by JawadTechX`
+                    caption: `📷 Result for: ${query}\n> © Powered by ObedTechX`
                 },
                 { quoted: mek }
             );
