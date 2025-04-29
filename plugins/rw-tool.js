d7f34d1e2e31c76d879df73808ae499df51ad46a
Obed tech <obedomoke18@gmail.com>
2025-04-29 10:24:06 +0300

    Update rw-tool.js

--- plugins/rw-tool.js
+++ plugins/rw-tool.js
@@ -17,7 +17,7 @@ cmd({
     const { data } = await axios.get(apiUrl);
     
     if (data.status && data.imgUrl) {
-      const caption = `🌌 *Random Wallpaper: ${query}*\n\n> *© Powered by Jawad TechX*`;
+      const caption = `🌌 *Random Wallpaper: ${query}*\n\n> *© Powered by Obed TechX*`;
       await conn.sendMessage(from, { image: { url: data.imgUrl }, caption }, { quoted: m });
     } else {
       reply(`❌ No wallpaper found for *"${query}"*.`);
